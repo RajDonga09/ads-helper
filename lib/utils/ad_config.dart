91a496da59660a3283b87611468e5904b6c90060
Raj Donga <rajdonga@xrstudio.in>
2022-04-11 16:17:50 +0530

    - remove native ad

--- lib/utils/ad_config.dart
+++ lib/utils/ad_config.dart
@@ -1,25 +1,28 @@
+import 'dart:developer';
+
 import 'package:ads_helper/interstitial_ad/interstitial_ad.dart';
 import 'package:ads_helper/rewarded_ad/rewarded_ad.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdConfig {
-  static late String adMobBannerAdUnitId;
-  static late String facebookBannerAdUnitId;
-  static late String adMobInterstitialAdUnitId;
-  static late String facebookInterstitialAdAdUnitId;
-  static late String adMobRewardedAdUnitId;
-  static late bool isShowFacebookBannerAds;
-  static late bool isShowFacebookInterstitialAd;
-  static late bool isProFeatureEnable;
-  static late int coolDownsTaps;
-  static late bool isAdFeatureEnable;
+  static late String adMobBannerAdUnitId; // admob_banner_ad_unitId
+  static late String facebookBannerAdUnitId; // facebook_banner_ad_unitId
+  static late String adMobInterstitialAdUnitId; // admob_interstitial_ad_unitId
+  static late String facebookInterstitialAdAdUnitId; // facebook_interstitial_ad_unitId
+  static late String adMobRewardedAdUnitId; // admob_rewarded_ad_unitId
+  static late bool isShowFacebookBannerAds; // isShow_facebook_banner_ads
+  static late bool isShowFacebookInterstitialAd; // isShow_facebook_interstitial_ad
+  static late bool isProFeatureEnable; // isPro_feature_enable
+  static late int coolDownsTaps; // cool_downs_taps
+  static late bool isAdFeatureEnable; // isAd_feature_enable
 
   // static int firstCoolDowns = 30;
   // static int secondCoolDowns = 60;
 
-  static init({
+  Future<void> init({
     required String adMobBannerId,
     required String faceBookBannerId,
-    required String adMobBannerInterstitialAdId,
+    required String adMobInterstitialAdId,
     required String faceBookInterstitialAdId,
     required String adMobRewardAdId,
     required bool proFutureEnable,
@@ -32,7 +35,7 @@ class AdConfig {
   }) async {
     adMobBannerAdUnitId = adMobBannerId;
     facebookBannerAdUnitId = faceBookBannerId;
-    adMobInterstitialAdUnitId = adMobBannerInterstitialAdId;
+    adMobInterstitialAdUnitId = adMobInterstitialAdId;
     facebookInterstitialAdAdUnitId = faceBookInterstitialAdId;
     adMobRewardedAdUnitId = adMobRewardAdId;
     isShowFacebookBannerAds = isShowFaceBookBannerAd;
@@ -43,6 +46,9 @@ class AdConfig {
     coolDownsTaps = coolDownsTap;
     // if (firstCoolDown != null) firstCoolDowns = firstCoolDown;
     // if (firstCoolDown != null) secondCoolDowns = firstCoolDown;
+
+    log("AdDetails adMobBannerAdUnitId: $adMobBannerAdUnitId facebookBannerAdUnitId: $facebookBannerAdUnitId");
+    MobileAds.instance.initialize();
 
     /// Load Ads
     if (isAdFeatureEnable) {
