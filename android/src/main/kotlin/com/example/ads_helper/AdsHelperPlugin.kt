090ac56ec421fcb53ab764a92b302df9dfd17d28
Raj Donga <rajdonga@xrstudio.in>
2022-04-11 17:53:12 +0530

    - fix android issue

--- android/src/main/kotlin/com/example/ads_helper/AdsHelperPlugin.kt
+++ android/src/main/kotlin/com/example/ads_helper/AdsHelperPlugin.kt
@@ -1,6 +1,7 @@
 package com.example.ads_helper
 
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -8,45 +9,29 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.MethodChannel.Result
 
-/** AdsHelperPlugin */
-class AdsHelperPlugin : FlutterPlugin, ActivityAware, MethodCallHandler {
+class AdsHelperPlugin : FlutterPlugin, MethodCallHandler {
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
     /// when the Flutter Engine is detached from the Activity
-    private var channel: MethodChannel? = null
-    override fun onAttachedToEngine(flutterPluginBinding: FlutterPluginBinding) {
+    private lateinit var channel: MethodChannel
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "ads_helper")
-        channel!!.setMethodCallHandler(this)
+        channel.setMethodCallHandler(this)
     }
 
-    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-        when (call.method) {
-            "initialize" -> {
-                result.success(true)
-            }
-            else -> result.notImplemented()
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else {
+            result.notImplemented()
         }
     }
 
-    override fun onDetachedFromEngine(binding: FlutterPluginBinding) {
-        channel!!.setMethodCallHandler(null)
-    }
-
-    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onDetachedFromActivityForConfigChanges() {
-        TODO("Not yet implemented")
-    }
-
-    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onDetachedFromActivity() {
-        TODO("Not yet implemented")
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
     }
 }
